1a74c8b981f4a12e593090dd19d0efd2457d3756
Rafael Santos <rafa.05125@gmail.com>
2026-03-20 20:53:03 -0300

    Fail fast validation

--- PaymentContext.Domain/Commands/CreateBoletoSubscriptionCommand.cs
+++ PaymentContext.Domain/Commands/CreateBoletoSubscriptionCommand.cs
@@ -1,16 +1,19 @@
+using Flunt.Notifications;
+using Flunt.Validations;
 using PaymentContext.Domain.Enums;
 using PaymentContext.Domain.ValueObjects;
+using PaymentContext.Shared.Commands;
 
 namespace PaymentContext.Domain.Commands
 {
-    public class CreateBoletoSubscriptionCommand
+    public class CreateBoletoSubscriptionCommand : Notifiable, ICommand
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public Document Document { get; set; }
         public string Email { get; set; }
 
-       
+
         public string BarCode { get; private set; }
         public string BoletoNumber { get; private set; }
         public string PaymentNumber { get; set; }
@@ -29,5 +32,14 @@ namespace PaymentContext.Domain.Commands
         public string State { get; set; }
         public string Country { get; set; }
         public string ZipCode { get; set; }
+
+        public void Validate()
+        {
+            AddNotifications(new Contract().Requires()
+                     .HasMinLen(FirstName, 3, "Name.FirstName", "Nome deve conter pelo menos 3 caracteres")
+                     .HasMinLen(LastName, 3, "Name.FirstName", "Sobrenome deve conter pelo menos 3 caracteres")
+                     .HasMaxLen(FirstName, 40, "Name.FirstName", "Nome deve conter até 40 caracteres")
+              );
+        }
     }
 }
