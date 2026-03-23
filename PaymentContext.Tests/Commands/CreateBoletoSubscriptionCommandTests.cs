1b6f560085ff480ee0e0037c21fbbea330b64a94
Rafael Santos <rafa.05125@gmail.com>
2026-03-23 09:55:28 -0300

    Testing commands

---
+++ PaymentContext.Tests/Commands/CreateBoletoSubscriptionCommandTests.cs
@@ -0,0 +1,20 @@
+using PaymentContext.Domain.Commands;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.ValueObjects;
+
+namespace PaymentContext.Tests;
+
+[TestClass]
+public class CreateBoletoSubscriptionCommandTests
+{
+    [TestMethod]
+    public void ShouldReturnErrorWhenNameInvalid()
+    {
+        var command = new CreateBoletoSubscriptionCommand();
+        command.FirstName = "";
+
+        command.Validate();
+        Assert.AreEqual(false, command.Valid);
+    }
+}
+
